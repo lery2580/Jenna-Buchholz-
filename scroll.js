733f20ad66b68c9748c32404de571f21673f1da1
lery2580 <73690399+lery2580@users.noreply.github.com>
2021-06-19 22:39:33 +0800

    first commit

--- scroll.js
+++ scroll.js
@@ -1,5 +1,9 @@
 const pixelsTag = document.querySelector("div.pixels")
 
+const bodyTag = document.querySelector("body")
+
+const progressTag = document.querySelector("div.progress")
+
 // when we scroll the page, update the pixels tag to be how far we scrolled
 
 document.addEventListener("scroll", function(){
@@ -10,5 +14,13 @@ document.addEventListener("scroll", function(){
 })
 
 document.addEventListener("scroll", function(){
+
     const pixels = window.pageYOffset
+    const pageHeight = bodyTag.getBoundingClientRect().height
+    const totalScrollableDistance = pageHeight - window.innerHeight
+
+    const percentage = pixels / totalScrollableDistance
+
+    progressTag.style.width = `${100 *percentage}%`
+
 })
