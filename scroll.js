2b7dc8e40d3c6e991863382e670625013c7454d5
lery2580 <73690399+lery2580@users.noreply.github.com>
2021-06-20 20:08:29 +0800

    first commit

--- scroll.js
+++ scroll.js
@@ -1,8 +1,10 @@
 const pixelsTag = document.querySelector("div.pixels")
-
 const bodyTag = document.querySelector("body")
-
 const progressTag = document.querySelector("div.progress")
+const sections = document.querySelectorAll("section")
+const clientTag = document.querySelector("div.client")
+const pageTag = document.querySelector("div.page")
+const headerTag = document.querySelector("header")
 
 // when we scroll the page, update the pixels tag to be how far we scrolled
 
@@ -22,5 +24,29 @@ document.addEventListener("scroll", function(){
     const percentage = pixels / totalScrollableDistance
 
     progressTag.style.width = `${100 *percentage}%`
+
+})
+
+// when we scroll the page, see how far down the page we've scrolled
+// then for each section, check whether we've passed if
+// then updated the text in the header
+
+document.addEventListener("scroll", function(){
+    const pixels = window.pageYOffset
+
+    sections.forEach(section => {
+        if (section.offsetTop -100 <= pixels) {
+            clientTag.innerHTML = section.getAttribute("data-client")
+            pageTag.innerHTML = section.getAttribute("data-page")
+
+            if (section.hasAttribute("data-is-dark")) {
+                headerTag.classList.add("white")
+                progressTag.classList.add("white")
+            } else {
+                headerTag.classList.remove("white")
+                progressTag.classList.remove("white")
+            }
+        }
+    })
 
 })
